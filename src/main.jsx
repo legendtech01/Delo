17350d38bd96963d1a37ef827390a902a7727bcf
legend <legendprosper2125@gmail.com>
2025-05-18 14:24:45 +0100

    added index css

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client'
 import ReactDom from "react-dom/client"
 import './index.css'
 import App from './App.jsx'
+import "./"
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
